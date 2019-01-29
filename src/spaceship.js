404f7b083c16637771fa10216eac2e0f1d5e05a6
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-29 21:27:23 +0000

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,23 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    if (this.crew.length === 0) {
+      this.docked = true;
+    } else {
+      this.docked = false;
+    }
+    this.phasersCharge = 'uncharged';
+    this.assignCrew();
+  }
+
+  assignCrew() {
+    for (var i = 0; i < this.crew.length; i++) {
+      this.crew[i].currentShip = this
+    }
+  }
+}
